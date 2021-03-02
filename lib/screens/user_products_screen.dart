128063b012934388b1e9445e7effd1885490747e
James <jemcmullin@gmail.com>
2021-03-01 21:03:42 -0600

    Add Products refresh on User Products Screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -7,6 +7,11 @@ import '../widgets/app_drawer.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
+
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -22,15 +27,20 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
           padding: const EdgeInsets.all(8),
           child: ListView.builder(
-              itemCount: productsData.items.length,
-              itemBuilder: (_, index) => UserProductItem(
-                    id: productsData.items[index].id,
-                    title: productsData.items[index].title,
-                    imageUrl: productsData.items[index].imageUrl,
-                  ))),
+            itemCount: productsData.items.length,
+            itemBuilder: (_, index) => UserProductItem(
+              id: productsData.items[index].id,
+              title: productsData.items[index].title,
+              imageUrl: productsData.items[index].imageUrl,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
