1b6ecdfc871d703d32bc969a4507acda03978615
jemcmullin <jemcmullin@gmail.com>
2021-03-30 20:25:01 -0500

    Refactor to CustomScroll with SliverAppBar

--- lib/screens/product_item_detail_screen.dart
+++ lib/screens/product_item_detail_screen.dart
@@ -10,17 +10,17 @@ class ProductItemDetailScreen extends StatelessWidget {
     final currentProduct =
         Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(currentProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
+      // appBar: AppBar(
+      //   title: Text(currentProduct.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(currentProduct.title),
+              background: Hero(
                 tag: currentProduct.id,
                 child: Image.network(
                   currentProduct.imageUrl,
@@ -28,25 +28,35 @@ class ProductItemDetailScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text('\$${currentProduct.price}',
-                style: TextStyle(fontSize: 20, color: Colors.grey[600])),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: double.infinity,
-              child: Text(
-                currentProduct.description,
-                style: TextStyle(fontSize: 15),
-                softWrap: true,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '\$${currentProduct.price}',
+                style: TextStyle(fontSize: 20, color: Colors.grey[600]),
                 textAlign: TextAlign.center,
               ),
-            ),
-          ],
-        ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                child: Text(
+                  currentProduct.description,
+                  style: TextStyle(fontSize: 15),
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 800,
+              ) //HACK: to demo scrolling SliverAppBar
+            ]),
+          )
+        ],
       ),
     );
   }
